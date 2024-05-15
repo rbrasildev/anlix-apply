e1a066d269921fbca74e0203b09783eb09e97f58
rbrasildev <rairockbrasil@hotmail.com>
2024-05-15 20:12:17 -0300

    last update

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import Routes from './src/routes';
 import { PaperProvider } from 'react-native-paper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
 
 
 export default function App() {
   return (
 
     <PaperProvider>
-      <Routes />
+      <GestureHandlerRootView>
+        <Routes />
+      </GestureHandlerRootView>
     </PaperProvider>
   );
 }
